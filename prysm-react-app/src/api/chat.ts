f1ad81d3d5ed488b3e76965761998ea73ba9fc23
f3liz <felixchen14@gmail.com>
2026-02-14 17:40:41 -0800

    created api folder, added chat.ts for chat api function

---
+++ prysm-react-app/src/api/chat.ts
@@ -0,0 +1,21 @@
+export async function sendChatQuestion(question: string): Promise<string> {
+    try {
+        const response = await fetch("route", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ question })
+        });
+
+        if (!response.ok) {
+            throw new Error("Error with backend!")
+        }
+
+        const data = await response.json();
+        return data.answer
+    } catch (error) {
+        console.error("Question failed to send: ", error)
+        throw error
+    }
+}
