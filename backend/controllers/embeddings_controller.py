f3997e9fb2af709bb34aed1b321844da48da54c7
BolshiAlex <145608606+Bolshialex@users.noreply.github.com>
2026-02-20 11:56:55 -0800

    Add temp comments to better understand the code logic

--- backend/controllers/embeddings_controller.py
+++ backend/controllers/embeddings_controller.py
@@ -8,13 +8,18 @@ async def embeddings_process(file: UploadFile = File()):
             raise HTTPException(status_code=400, detail="File must be a PDF")
 
     try:
+        #pdf_content takes the file and turns it into 1 and 0
         pdf_content = await file.read()
+
+        # doc is now an object that understands pdf structure like pages, fonts, images
         doc = pymupdf.open(stream=pdf_content, filetype="pdf")
         
+        # gets all text from each page in doc
         full_text = ""
         for page in doc:
             full_text += page.get_text()
 
+        # chunks is an array of each 500 characters
         chunks = chunk_processing.chunk_text(full_text)
 
     except Exception as e:
