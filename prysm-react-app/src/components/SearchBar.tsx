b111616a7a1eb7b9c5ee76033716e97a95942e8e
Kimberly Mageary <kimberlymageary@gmail.com>
2026-02-23 18:22:03 -0500

    Updated search algorithm to now check within the answers.

--- prysm-react-app/src/components/SearchBar.tsx
+++ prysm-react-app/src/components/SearchBar.tsx
@@ -1,17 +1,38 @@
 import { prysmFaqData } from "../data/prysmFaqData.ts";
+import type { SearchBarProps } from "../types.ts";
 import { useState, useMemo } from "react";
 
-export function SearchBar({onSearch} : any){
+export function SearchBar({onSearch} : SearchBarProps){
     const [searchTerm, setSearchTerm] = useState("");
 
     useMemo(() => {
+        /** Below is an algorithm breaking up and searching this object for any matches
+         * {
+         *      id: Number,
+         *      question: String,
+         *      sections: [{
+         *         title: String,
+         *         content: String[] 
+         *      }]
+         * }
+         */
         const value = searchTerm.toLocaleLowerCase().trim();
 
         const filtered = value
             ? prysmFaqData.filter((faq) => {
                 // Check against questions first
-                if (faq.question.toLowerCase().includes(value)) return true;
+                const matchingQuestions = faq.question.toLowerCase().includes(value);
 
+                // Check for any matching sections
+                const matchSections = faq.sections.some((section) => {
+                    const titleMatch = section.title.toLowerCase().includes(value);
+                    const contentMatch = section.content.some((content) => {
+                        return content.toLowerCase().includes(value)
+                    })
+                    return titleMatch || contentMatch
+                });
+
+                return matchingQuestions || matchSections;
                 
             }) : prysmFaqData
 
