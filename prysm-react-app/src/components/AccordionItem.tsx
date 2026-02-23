84ce8c94f84833f1aec7dda73c2551db7e4b745f
Kimberly Mageary <kimberlymageary@gmail.com>
2026-02-23 18:23:33 -0500

    Fixed all Type issues, there are no more types set to "any".

--- prysm-react-app/src/components/AccordionItem.tsx
+++ prysm-react-app/src/components/AccordionItem.tsx
@@ -1,11 +1,7 @@
-type AccordionItemProps = {
-    title: string;
-    content: string;
-    isExpanded: boolean;
-    onToggle: () => void;
-};
+import type { AccordionItemProps, Section } from "../types"
 
-export default function AccordionItem({title, content, isExpanded, onToggle}: AccordionItemProps) 
+
+export default function AccordionItem({title, data, isExpanded, onToggle}: AccordionItemProps) 
 {
     return(
         <div className="accordion-item">
@@ -18,9 +14,8 @@ export default function AccordionItem({title, content, isExpanded, onToggle}: Ac
 
             {isExpanded && (
                 <div className="accordion-content">
-                {content}
                 </div>
-            )}
+      )}
         </div>
     )
 }
