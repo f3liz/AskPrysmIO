0f432be852846b1ddc6371b3563110b3a71fbeb5
JesseChum <jessechum@icloud.com>
2026-03-02 11:12:12 -0800

    installed vitetest and setup for testing. commands work. faq.test.tsx is the main test file

--- prysm-react-app/vite.config.ts
+++ prysm-react-app/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: resolve(__dirname, "src/test/setup.ts"),
+  },
+});
