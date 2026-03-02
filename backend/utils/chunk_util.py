a4a6cfd4fe690a51f517a68e6704d5bcaba42c72
f3liz <felixchen14@gmail.com>
2026-03-02 14:26:34 -0800

    implemented chunk_util to chunk texts into tokens for embedding

--- backend/utils/chunk_util.py
+++ backend/utils/chunk_util.py
@@ -1,3 +1,18 @@
-def chunk_text(text: str, chunk_size: int = 500):
+import tiktoken
+
+encode = tiktoken.encoding_for_model("text-embedding-3-large")
+
+def chunk_text(text: str, max_tokens: int = 800, overlap: int = 200):
     #This is an example of chunk automation. Creating a new chunk every 500 characters
-    return [text[i : i + chunk_size] for i in range(0, len(text), chunk_size)]
+    tokens = encode.encode(text)
+    chunks = []
+    start = 0
+
+    while start < len(tokens):
+        end = start + max_tokens
+        chunk_tokens = tokens[start:end]
+        chunk_text = encode.decode(chunk_tokens)
+        chunks.append(chunk_text)
+        start += max_tokens - overlap
+    
+    return chunks
