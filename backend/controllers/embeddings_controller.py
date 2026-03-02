654dee333ee4b6eafa3cb8c5332fb1916268133b
f3liz <felixchen14@gmail.com>
2026-03-02 15:24:43 -0800

    added code for embeddings_process logic to insert our embeddings and other information to supabase db

--- backend/controllers/embeddings_controller.py
+++ backend/controllers/embeddings_controller.py
@@ -1,21 +1,37 @@
-import pymupdf
-from fastapi import UploadFile, File, HTTPException
-from typing import List
-from utils import  pdf_util, chunk_util
+from fastapi import UploadFile, File, HTTPException, Form
+from utils import  pdf_util, chunk_util, embedding_util
+from db import supabase
 
-async def embeddings_process(file: UploadFile = File()):
+async def embeddings_process(file: UploadFile = File(), title: str = Form(...)):
     if not file.filename.endswith(".pdf"):
-            raise HTTPException(status_code=400, detail="File must be a PDF")
+        raise HTTPException(status_code=400, detail="File must be a PDF")
 
     try:
         pdf_content = await file.read()
         pages = pdf_util.extract_text(pdf_content)
+
+        total_chunks = 0
+        chunk_index = 0
         
         for page in pages:
-             chunks = chunk_util.chunk_text(page["content"])
+            chunks = chunk_util.chunk_text(page["content"])
+            embeddings = embedding_util.text_embedding(chunks)
+
+            for chunk, embedding in zip(chunks, embeddings):
+                supabase.table("pdfdocuments").insert({
+                    "title" : title,
+                    "source" : file.filename,
+                    "content" : chunk,
+                    "page_number" : page["page_number"],
+                    "chunk_index" : chunk_index,
+                    "embedding" : embedding
+                }).execute()
+
+                chunk_index += 1
+
         return{
-             "Pages": pages,
-             "Chunk": chunks
+            "Pages": pages,
+            "Chunk": chunks
         }
 
     except Exception as e:
