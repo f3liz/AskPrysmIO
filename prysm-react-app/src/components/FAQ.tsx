bd3e9ed023098ba8f3c4516003b256602331573b
Kimberly Mageary <kimberlymageary@gmail.com>
2026-03-02 18:28:30 -0500

    Fixed state-based rendering logic.

--- prysm-react-app/src/components/FAQ.tsx
+++ prysm-react-app/src/components/FAQ.tsx
@@ -7,42 +7,27 @@ import "../styles/faq.css"
 export default function FAQ() {
     const [isExpanded, setExpandedId] = useState<number | null>(null);
     const [filteredFaqs, setFilteredFaqs] = useState(prysmFaqData);
-    const [searchQuery, setSearchQuery] = useState("");
+
 
     const handleToggle = (id: number) => {
         setExpandedId(isExpanded === id ? null : id);
     };
-    //tracks input
-    const handleSearch = (results: typeof prysmFaqData, query: string) => {
-        setFilteredFaqs(results);
-        setSearchQuery(query);
-    }
+    
 
     return(
         <div className="faq-container">
-            <SearchBar onSearch={handleSearch} />
+            <SearchBar onSearch={setFilteredFaqs} />
             <div className="faq-content">
                 {/* Empty Search State function*/}
-                {searchQuery === "" && (
-                    filteredFaqs.map((faq) => (
-                         <AccordionItem
-                            key={faq.id}
-                            title={faq.question}
-                            data={faq.sections}
-                            isExpanded={isExpanded === faq.id}
-                            onToggle={() => handleToggle(faq.id)}
-                        />
-                    ))
-                )}
                     {/*No Results found state function*/}
-                    {searchQuery !== "" && filteredFaqs.length === 0 && (
+                    {filteredFaqs.length === 0 && (
                         <p className="no-results">
                             Please refer to Ask PrysmIO
                         </p>
                     )}
 
                 {/*Search result state function */}
-                {searchQuery !== "" && filteredFaqs.length > 0 && (
+                {filteredFaqs.length > 0 && (
                     filteredFaqs.map((faq) => (
                     <AccordionItem
                         key={faq.id}
