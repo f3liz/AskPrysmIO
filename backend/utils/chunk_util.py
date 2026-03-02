65607887e3d60542051045b5d6ae461a3a9ff2a9
f3liz <felixchen14@gmail.com>
2026-03-02 15:43:04 -0800

    removed comment

--- backend/utils/chunk_util.py
+++ backend/utils/chunk_util.py
@@ -3,7 +3,7 @@ import tiktoken
 encode = tiktoken.encoding_for_model("text-embedding-3-large")
 
 def chunk_text(text: str, max_tokens: int = 800, overlap: int = 200):
-    #This is an example of chunk automation. Creating a new chunk every 500 characters
+    
     tokens = encode.encode(text)
     chunks = []
     start = 0
