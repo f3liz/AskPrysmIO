a3a0ad0c434d1fcd7c5920604ada96e4fae0f792
BolshiAlex <145608606+Bolshialex@users.noreply.github.com>
2026-03-02 14:58:19 -0800

    Add chunk_text to embeddings controller so that we can chunk up the text

--- backend/controllers/embeddings_controller.py
+++ backend/controllers/embeddings_controller.py
@@ -1,7 +1,7 @@
 import pymupdf
 from fastapi import UploadFile, File, HTTPException
 from typing import List
-from utils import  pdf_util
+from utils import  pdf_util, chunk_util
 
 async def embeddings_process(file: UploadFile = File()):
     if not file.filename.endswith(".pdf"):
@@ -10,9 +10,12 @@ async def embeddings_process(file: UploadFile = File()):
     try:
         pdf_content = await file.read()
         pages = pdf_util.extract_text(pdf_content)
-
+        
+        for page in pages:
+             chunks = chunk_util.chunk_text(page["content"])
         return{
-             "Pages": pages
+             "Pages": pages,
+             "Chunk": chunks
         }
 
     except Exception as e:
