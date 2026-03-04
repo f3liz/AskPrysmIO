76a7d49a2c601c313a8efbe274fad2fe8c330069
f3liz <felixchen14@gmail.com>
2026-03-04 11:43:31 -0800

    refactored to 1 embedding function and wrappers for the functions

--- backend/utils/embedding_util.py
+++ backend/utils/embedding_util.py
@@ -3,18 +3,19 @@ from config import settings
 
 client = OpenAI(api_key=settings.OPENAI_EMBEDDING_KEY)
 
-def text_embedding(text_chunks: list[str]) -> list[list[float]]:
+def embed_text(input_text: str | list[str]):
     response = client.embeddings.create(
         model="text-embedding-3-small",
-        input=text_chunks
+        input=input_text
     )
 
-    return [item.embedding for item in response.data]
+    if isinstance(input_text, list):
+        return [item.embedding for item in response.data]
+    
+    return response.data[0].embedding
 
 def question_embedding(question: str) -> list[float]:
-    response = client.embeddings.create(
-        model="text-embedding-3-small",
-        input=question
-    )
+    return embed_text(question)
 
-    return response.data[0].embedding
+def text_embedding(text_chunks: list[str]) -> list[list[float]]:
+    return embed_text(text_chunks)
