7ef7e304ad510d2f04a92260231eec664a6c920e
f3liz <felixchen14@gmail.com>
2026-03-04 13:43:15 -0800

    fixed typos

--- backend/utils/question_retrieval_util.py
+++ backend/utils/question_retrieval_util.py
@@ -1,13 +1,13 @@
 from db import supabase
-from embedding_util import question_embedding
+from .embedding_util import question_embedding
 
 async def search_docs(question: str, matches: int = 5, threshold = 0.75):
-    embedding = await question_embedding(question)
+    embedding = question_embedding(question)
 
     response = supabase.rpc(
-        "match_docments",
+        "match_documents",
         {
-            "question_embedding" : question_embedding,
+            "query_embedding" : embedding,
             "match_threshold" : threshold,
             "match_count" : matches
         }
