4005ed7bb235cda57ffb91930783091eac6da05d
f3liz <felixchen14@gmail.com>
2026-03-03 18:10:07 -0800

    added route prefix for embedding and changed post route to embed

--- backend/routers/embeddings.py
+++ backend/routers/embeddings.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, UploadFile, File, Form
 from controllers import embeddings_controller # Adjust path to your controller
 
-router = APIRouter()
+router = APIRouter(prefix="/embeddings", tags=["embeddings"])
 
-@router.post("/embeddings")
+@router.post("/embed")
 async def upload_file(file: UploadFile = File(...), title: str = Form(...)):
     # Pass the file to your controller method
     result = await embeddings_controller.embeddings_process(file, title)
