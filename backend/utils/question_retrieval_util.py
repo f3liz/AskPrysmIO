f89e642d19eebfa36ef2e55c23bcd701c665b430
f3liz <felixchen14@gmail.com>
2026-03-04 11:55:44 -0800

    finished question retreival util to send question to db and retreive relevant chunks. Put it all together in chat controller

--- backend/utils/question_retrieval_util.py
+++ backend/utils/question_retrieval_util.py
@@ -1,7 +1,16 @@
 from db import supabase
 from embedding_util import question_embedding
 
-async def search_docs(question: str, matches: int = 5):
+async def search_docs(question: str, matches: int = 5, threshold = 0.75):
     embedding = await question_embedding(question)
 
-    response = supabase
+    response = supabase.rpc(
+        "match_docments",
+        {
+            "question_embedding" : question_embedding,
+            "match_threshold" : threshold,
+            "match_count" : matches
+        }
+    ).execute()
+
+    return response.data
