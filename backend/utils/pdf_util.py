d8af2ce4ddc947dde0960cdf88f7970d206c3cb3
f3liz <felixchen14@gmail.com>
2026-03-11 11:39:02 -0700

    started a function to clean up pdf text data

--- backend/utils/pdf_util.py
+++ backend/utils/pdf_util.py
@@ -1,4 +1,10 @@
 import pymupdf
+import re
+
+def clean_pdf_text(text: str) -> str:
+    text = text.replace("\r", "\n")
+
+    text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
 
 def extract_text(pdf_bytes: bytes):
     doc = pymupdf.open(stream=pdf_bytes, filetype="pdf")
