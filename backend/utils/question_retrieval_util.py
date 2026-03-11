fc106caa7daa5cf0b63ca5a97380f396751bfb46
f3liz <felixchen14@gmail.com>
2026-03-11 12:21:30 -0700

    created question cleanup function

--- backend/utils/question_retrieval_util.py
+++ backend/utils/question_retrieval_util.py
@@ -1,5 +1,13 @@
 from db import supabase
 from .embedding_util import question_embedding
+import re
+
+def question_cleanup(question: str) -> str:
+    question = question.strip()
+
+    question = re.sub(r"\s+", " ", question)
+
+    return question
 
 async def search_docs(question: str, matches: int = 5, threshold = 0.75):
     embedding = question_embedding(question)
