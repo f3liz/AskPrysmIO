1aee4d339d652e5381e8e423fef2e5906f4de022
Kimberly Mageary <kimberlymageary@gmail.com>
2026-04-20 16:26:54 -0700

    Wrote integration tests for require_auth function

--- backend/tests/integration/test_auth_controller.py
+++ backend/tests/integration/test_auth_controller.py
@@ -1,11 +1,50 @@
-from fastapi import FastAPI, Depends
+import pytest, jwt, os
+from pydantic_settings import BaseSettings
 from fastapi.testclient import TestClient 
 from backend.main import app
 
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
 client = TestClient(app)
+
+def create_test_token():
+    return jwt.encode(
+        {"sub": "test"},
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
 
 def test_that_i_can_test():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Backend running"}
+    assert response.json() == {"message":"Backend running"}
+
+def test_require_auth_success():
+    token = create_test_token()
+
+    response = client.get(
+        "/check/",
+        cookies={"access_token": token}
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {"message":"Successfully authenticated!"}
+        
+def test_require_auth_missing_token():
+    response = client.get("/check/")
+
+    assert response.status_code == 401
+    assert response.json() == {"detail":"Not authenticated"}
+
+def test_require_auth_invalid_token():
+    response = client.get(
+        "/check/",
+        cookies={"access_token": "scs2e"}
+    )
+
+    assert response.status_code == 401
+    assert response.json() == {"detail":"Invalid or expired token"}
+
+
 
