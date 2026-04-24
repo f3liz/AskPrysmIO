88c26c1dcaccb04efba94faa4d9792716f039115
Kimberly Mageary <kimberlymageary@gmail.com>
2026-04-24 12:10:10 -0700

    Implemented API route to refresh auth tokens.

--- backend/controllers/auth_controller.py
+++ backend/controllers/auth_controller.py
@@ -12,9 +12,17 @@ ADMIN_USER = os.getenv("ADMIN_USER")
 ADMIN_PASSWORD_PLAIN = os.getenv("ADMIN_PASSWORD")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+ACCESS_TOKEN_EXPIRE = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 pre_hashed_admin_pwd = hashlib.sha256(ADMIN_PASSWORD_PLAIN.encode('utf-8')).hexdigest().encode('utf-8')
 ADMIN_PASSWORD_HASH = bcrypt.hashpw(pre_hashed_admin_pwd, bcrypt.gensalt())
+
+def create_refresh_token(data: dict):
+    expires = timedelta(days=ACCESS_TOKEN_EXPIRE)
+    return create_token(data, expires, "refresh")
+
+def create_access_token(data: dict):
+    expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE)
+    return create_token(data, expires, "access")
 
 def require_auth(request: Request):
     token = request.cookies.get("access_token")
@@ -44,11 +52,42 @@ def verify_password(plain_password: str, hashed_password: bytes):
         
     return bcrypt.checkpw(pre_hashed_attempt, hashed_password)
 
-def create_access_token(data: dict, expires_delta: timedelta):
+def create_token(data: dict, expires_delta: timedelta, token_type: str):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + expires_delta
-    to_encode.update({"exp": expire})
+    to_encode.update({"exp": expire, "type": token_type})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def refresh_logic(request: Request, response: Response):
+    refresh_token = request.cookies.get("refresh_token")
+    if not refresh_token:
+        raise HTTPException(status_code=401, detail="Refresh token missing")
+
+    try:
+        payload = jwt.decode(refresh_token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        if payload.get("type") != "refresh":
+            raise HTTPException(status_code=401, detail="Invalid token type")
+        
+        username = payload.get("sub")
+
+        new_access_token = create_access_token(data={"sub": username})
+
+        response.set_cookie(
+            key="access_token",
+            value=new_access_token,
+            httponly=True,
+            secure=False,
+            samesite="lax",
+            max_age=ACCESS_TOKEN_EXPIRE * 60
+        )
+
+        return {"message": "Token refreshed"}
+    except JWTError:
+        raise HTTPException(
+            status_code=401,
+            detail="Error refreshing token."
+        )
 
 def process_login(username: str, password: str, response: Response):
     if username != ADMIN_USER or not verify_password(password, ADMIN_PASSWORD_HASH):
@@ -57,18 +96,28 @@ def process_login(username: str, password: str, response: Response):
             detail="Incorrect username or password",
         )
 
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": username}, expires_delta=access_token_expires
+        data={"sub": username}
+    )
+    refresh_token = create_refresh_token(
+        data={"sub":username}
     )
 
     response.set_cookie(
         key="access_token",
-        value=f"Bearer {access_token}",
+        value=access_token,
         httponly=True,
         secure=False,
         samesite="lax",
-        max_age=ACCESS_TOKEN_EXPIRE_MINUTES * 60
+        max_age=ACCESS_TOKEN_EXPIRE * 60
+    )
+
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        path="/refresh",
+        max_age=7 * 24 * 60 * 60 # 7 days
     )
     return {"message": "Login successful"}
 
--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter, Response, Depends
+from fastapi import APIRouter, Response, Depends, Request
 from pydantic import BaseModel
-from backend.controllers.auth_controller import process_login, process_logout
+from backend.controllers.auth_controller import process_login, process_logout, refresh_logic
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -15,3 +15,7 @@ def login(request: LoginRequest, response: Response):
 @router.post("/logout")
 def logout(response: Response):
     return process_logout(response)
+
+@router.get("/refresh")
+async def refresh_token(request: Request, response: Response):
+    return refresh_logic(request, response)
