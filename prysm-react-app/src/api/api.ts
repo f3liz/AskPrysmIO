8fc3f328af785e078497390b233e641fbe50c776
Kimberly Mageary <kimberlymageary@gmail.com>
2026-04-25 09:44:42 -0700

    Configured axios for all API calls

---
+++ prysm-react-app/src/api/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_BASE = import.meta.env.VITE_API_BASE ?? "/api";
+export const api = axios.create({
+    baseURL: API_BASE,
+    headers: {'Content-Type': 'application/json'},
+    withCredentials: true
+})
