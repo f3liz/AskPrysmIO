c27d6e7272fa523806261a8c55dc68e8cf308bbf
Kimberly Mageary <kimberlymageary@gmail.com>
2026-04-27 14:41:37 -0700

    Added persistent authentication and response interceptor  rerouting 401 errors to the login page.

--- prysm-react-app/src/api/api.ts
+++ prysm-react-app/src/api/api.ts
@@ -1,8 +1,23 @@
 import axios from "axios";
+import type { AxiosResponse } from "axios";
 
 const API_BASE = import.meta.env.VITE_API_BASE ?? "/api";
+
 export const api = axios.create({
     baseURL: API_BASE,
     headers: {'Content-Type': 'application/json'},
     withCredentials: true
 })
+
+export const setupInterceptors = (navigate: any, setIsAuthenticated: (val: boolean) => void) => {
+    api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+    (error) => {
+        if (error.response?.status === 401){
+            navigate('/login', { replace: true })
+            setIsAuthenticated(false);
+        }
+        return Promise.reject(error);
+    }
+    );
+}
