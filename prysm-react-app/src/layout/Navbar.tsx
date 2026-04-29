8b1b8c674b772a045f29dd0c45dac6af6528e38f
bolshialex <145608606+Bolshialex@users.noreply.github.com>
2026-04-29 11:37:08 -0700

    Add styling fixes from migration

--- prysm-react-app/src/layout/Navbar.tsx
+++ prysm-react-app/src/layout/Navbar.tsx
@@ -1,23 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <header className="navbar">
+      <Link to="/" className="brand-link">
         <div className="brand">
           <span className="logoText">
             NU SKIN<sup>®</sup>
           </span>
         </div>
+      </Link>
 
-        <div className="btn-container">
-          <Link to="/faq">
-            <button className="navButton">Go to FAQ</button>
-          </Link>
-          <Link to="/admin">
-            <button className="navButton">File Upload</button>
-          </Link>
-        </div>
-      </header>
-  )
+      <div className="btn-container">
+        <NavLink 
+          to="/faq" 
+          className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}
+        >
+          <button className="navButton">Go to FAQ</button>
+        </NavLink>
+        
+        <NavLink 
+          to="/admin" 
+          className={({ isActive }) => isActive ? "nav-item active" : "nav-item"}
+        >
+          <button className="navButton">File Upload</button>
+        </NavLink>
+      </div>
+    </header>
+  );
 }
 
-export default Navbar
+export default Navbar;
